cd3882fe50f2a216414806c791a79dee9d56b21d
Shumin Wu <wushumin@gmail.com>
2016-02-24 15:10:02 -0700

    Changing clearsrl to maven project/modules

---
+++ liblinearbinary/src/main/java/edu/colorado/clear/liblinearbinary/Function.java
@@ -0,0 +1,13 @@
+package edu.colorado.clear.liblinearbinary;
+
+// origin: tron.h
+interface Function {
+
+    double fun(double[] w);
+
+    void grad(double[] w, double[] g);
+
+    void Hv(double[] s, double[] Hs);
+
+    int get_nr_variable();
+}
